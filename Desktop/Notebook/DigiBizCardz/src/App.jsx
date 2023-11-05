fec0f2937efd7475b674b9c9ec02685b40d80ab4
Solange <skouchy24@gmail.com>
2023-11-04 23:04:07 -0400

    feat(content): substance is there, main scaffolding. TODO: compose

--- Desktop/Notebook/DigiBizCardz/src/App.jsx
+++ Desktop/Notebook/DigiBizCardz/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import santaSolange from "./assets/santa.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className="card">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://github.com/skouchy" target="_blank" rel="noreferrer">
+          <img src={santaSolange} className="avatar" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
+      <main className="body">
+        <div className="heading">
+          <h1 className="myName">Solange Maya</h1>
+          <h2 className="role">funemployed</h2>
+          <h3 className="website">github.com/skouchy</h3>
+        </div>
+        <div className="gritty-content">
+          <div className="buttons">
+            <button className="btn btn-primary">
+              <i className="bi bi-envelope-fill" />
+              Email
+            </button>
+            <button className="btn btn-secondary">
+              <i className="bi bi-linkedin" />
+              LinkedIn
+            </button>
+          </div>
+          <h3 className="small-head">About Me</h3>
+          <p>
+            Here is some stuff under about me that has nothing to do with me at
+            all, just exclusive to whats happening here. goes to show, the title
+            doesnt tell the whole story, now does it?
+          </p>
+          <h3 className="small-head">Interests</h3>
+        </div>
+      </main>
+      <footer className="footer">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
